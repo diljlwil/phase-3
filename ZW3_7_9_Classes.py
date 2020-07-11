c1178710ea5efe0e0227dcc55f0680445e00f551
diljlwil <67410918+diljlwil@users.noreply.github.com>
2020-07-11 00:29:26 -0600

    Add files via upload

---
+++ ZW3_7_9_Classes.py
@@ -0,0 +1,64 @@
+
+
+
+def Worldsize(a, x, y):
+    rows, cols = (11,11)
+    a = []
+    for i in range(cols):
+        col = []
+        for j in range(rows):
+            col.append(object)
+        a.append(cols)
+    pass
+
+
+
+  
+def main():
+    pass
+
+
+if __name__ == 'main':
+    main()
+print' '
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class WorldZ():
+    PersonN = " "
+    PersonZ = " "
+    PersonIm = " "
+
+    def __init__
+    
+    
+
+
+
+
+    class PersonN():
+        def __init__(self, PersonN):
+            print('not a zomb')
+            PersonN = 'Normal'
+
+    class PersonZ():
+        def __init__(self, PersonZ):
+            PersonZ = 'first Zomb'
+            
+                                                                                                                                                                                                                                                                                                     self.PersonN = NotInfected
+        
+    
+
+    
+        
